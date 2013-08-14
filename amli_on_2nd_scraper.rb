5c1491437341ea3321727efc025738799ef0dbf9
Justin Litchfield <litch@me.com>
2013-08-13 19:54:03 -0500

    initial commit (ack)

---
+++ amli_on_2nd_scraper.rb
@@ -0,0 +1,67 @@
+require 'selenium-webdriver'
+require 'awesome_print'
+
+class String
+  def underscore
+    self.gsub(/\W+/,'_').
+    tr("-", "_").
+    downcase
+  end
+end
+
+
+class AmliScraper
+  attr_accessor :url, :property_name
+
+  def initialize(url, property_name)
+    self.url = url
+    self.property_name = property_name
+  end
+
+  def scrape
+    driver = Selenium::WebDriver.for :firefox
+
+    driver.navigate.to url
+    fp_element_count = driver.find_elements(id: 'fpHolder').size
+    fp_elements = driver.find_elements(id: 'fpHolder')
+
+    info = []
+    [*0..fp_element_count-1].each do |floor_plan_index|
+      fp = driver.find_elements(id: 'fpHolder')[floor_plan_index]
+      floor_plan_name = fp.text.strip.split(/\n/).first
+      square_footage = fp.text.strip.split(/\n/)[4]
+      bed_bath_count = fp.text.strip.split(/\n/)[3]
+      fp.find_element(class: 'FpItemButton').click
+      units = driver.find_element(class: 'tblSummary').find_elements(tag_name: 'tr')
+      headers = units.slice!(0,1).first
+      units.each_with_index do |unit, i|
+        unit_summary = {floor_plan_name: floor_plan_name, property_name: property_name, retrieved: Time.now, square_footage: square_footage, bed_bath_count: bed_bath_count}
+        unit.find_elements(tag_name: 'td').each_with_index do |cell, j|
+          unit_summary[headers.find_elements(tag_name: 'th')[j].text.strip.underscore.to_sym] = cell.text
+        end
+        info << unit_summary
+      end
+    end
+    driver.close
+    info
+  end
+
+end
+
+results = {}
+
+amlis = {
+  amli_on_2nd: "http://www.amli.com/apartments/austin/downtown/austin/2nd-street/floorplans",
+  amli_5350: "http://www.amli.com/apartments/austin/central-austin/austin/5350/floorplans",
+  amli_downtown: 'http://www.amli.com/apartments/austin/2nd-street-district/austin/downtown/floorplans',
+  amli_south_shore: 'http://www.amli.com/apartments/austin/central-east-austin/austin/south-shore/floorplans',
+  amli_lantana_hills: 'http://www.amli.com/apartments/austin/southwest-austin/austin/lantana-hills/floorplans',
+  amli_300: 'http://www.amli.com/apartments/austin/downtown/austin/300/floorplans',
+  amli_eastside: 'http://www.amli.com/apartments/austin/downtown/austin/eastside/floorplans'
+}
+
+amlis.each do |property_name, url|
+  results[property_name] = AmliScraper.new(url, property_name).scrape
+end
+
+File.open("scrape", 'w') { |file| file.write(results) }
---
+++ udr_scraper.rb
@@ -0,0 +1,49 @@
+ashton = {
+  ashton: 'https://www.udr.com/floorplan.aspx?pid=35351'
+}
+
+# https://property.onesite.realpage.com/oll/Unit/SearchSetup?siteId=2335204
+
+require 'selenium-webdriver'
+require 'awesome_print'
+
+class String
+  def underscore
+    self.gsub(/\W+/,'_').
+    tr("-", "_").
+    downcase
+  end
+end
+
+
+class UdrScraper
+  attr_accessor :url, :property_name
+
+  def initialize(url, property_name)
+    self.url = url
+    self.property_name = property_name
+  end
+
+  def scrape
+    driver = Selenium::WebDriver.for :firefox
+    driver.navigate.to url
+    wait = Selenium::WebDriver::Wait.new(:timeout => 10) # seconds
+    wait.until {driver.find_element(id: 'page-title')}
+    p "Looks like load is done!"
+    info = []
+    driver.find_elements(class: 'floorplan-description').each do |floorplan_cell|
+      floor_plan_name = floorplan_cell.find(tag_name: 'h2').text.split('-')[0].strip
+      bed_bath_count = floorplan_cell.find(tag_name: 'h2').text.split('-')[1].strip
+      floorplan_cell.find_element(tag_name: 'tbody').find_elements(tag_name: 'tr').each do |unit|
+        unit_summary = {floor_plan_name: floor_plan_name, property_name: property_name, retrieved: Time.now, square_footage: square_footage, bed_bath_count: bed_bath_count}
+        unit_summary['text'] = unit.text
+      end
+    end
+    driver.close
+    info
+  end
+
+end
+
+
+ap UdrScraper.new(ashton[:ashton], :ashton).scrape
